fd4df4b812e1a9d2167af6859de5436db3be8340
Daniel Kim <io.gomdopi@gmail.com>
2020-08-06 16:44:17 -0700

    make 'mkcd' a script instead of an alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,5 +1,4 @@
 #general
-alias mkcd='abc() { mkdir -p "$1"; cd "$1"; }; abc'
 alias hs='history | grep'
 alias myip='curl https://ipecho.net/plain; echo'
 alias update='sudo apt update && sudo apt upgrade'
--- zsh/scripts.zsh
+++ zsh/scripts.zsh
@@ -1,2 +1,13 @@
+#general
+mkcd() {
+	if [ ! -n "$1" ]; then
+		echo "Enter a directory name"
+	elif [ -d $1 ]; then
+		echo "\`$1' already exists"
+	else
+		mkdir -p $1 && cd $1
+	fi
+}
+
 #git
 gcr() { ~/dev/scripts/git-create-repo.sh $1; }
